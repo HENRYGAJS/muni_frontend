282e32307298a03c34a9a11ca9f94c95b0780d23
Henrysd74 <103168861+Henrysd74@users.noreply.github.com>
2024-10-20 12:47:34 -0600

    Cambio en Inicio
    Se realizo cambio para evitar errores cuando no se ha cargado nada en el apartado de inicio en el administrador de contenidos

--- municipalidad-de-cantel/src/pages/Inicio.jsx
+++ municipalidad-de-cantel/src/pages/Inicio.jsx
@@ -7,12 +7,10 @@ const Inicio = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [currentImages, setCurrentImages] = useState([]);
   
-  // Estado para almacenar las noticias más recientes
   const [noticiasRecientes, setNoticiasRecientes] = useState([]);
 
-  // Obtener los datos del inicio desde la API
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/inicio/')  // Asegúrate de que la URL sea correcta
+    axios.get('http://127.0.0.1:8000/api/inicio/')
       .then(response => {
         setInicioData(response.data);
       })
@@ -21,30 +19,26 @@ const Inicio = () => {
       });
   }, []);
 
-  // Obtener las noticias más recientes desde la API
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/noticias_recientes/')  // Petición para las noticias recientes
+    axios.get('http://127.0.0.1:8000/api/noticias_recientes/')
       .then(response => {
-        setNoticiasRecientes(response.data);  // Guardamos las noticias en el estado
+        setNoticiasRecientes(response.data);
       })
       .catch(error => {
         console.error('Hubo un error al obtener las noticias recientes:', error);
       });
   }, []);
 
-  // Función para abrir el modal y mostrar la primera imagen
   const openModal = (imagenes, index) => {
     setCurrentImages(imagenes);
     setCurrentImageIndex(index);
     setIsModalOpen(true);
   };
 
-  // Función para cerrar el modal
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
-  // Función para avanzar a la siguiente imagen
   const handleNextImage = () => {
     const nextIndex = (currentImageIndex + 1) % currentImages.length;
     setCurrentImageIndex(nextIndex);
@@ -56,40 +50,45 @@ const Inicio = () => {
 
   return (
     <div>
-      {/* Imagen de encabezado con texto */}
-      <div className="relative">
-        <img
-          src={`http://127.0.0.1:8000${inicioData.imagenes_palacio[0].imagen}`}  // Mostrar la primera imagen del palacio
-          alt="Municipalidad de Cantel"
-          className="w-full h-[60vh] object-cover cursor-pointer"
-          onClick={() => openModal(inicioData.imagenes_palacio, 0)}  // Abre el modal con las imágenes
-        />
-        <div className="absolute bottom-0 left-0 p-4 text-white bg-gradient-to-t from-black via-transparent to-transparent">
-          <h1 className="text-xl md:text-2xl lg:text-4xl font-semibold">
-            {inicioData.eslogan}  {/* Eslogan */}
-          </h1>
-          <p className="text-md md:text-lg lg:text-xl mt-1">
-            {inicioData.administracion}  {/* Administración */}
-          </p>
+      {/* Validación para asegurarse de que hay imágenes disponibles */}
+      {inicioData.imagenes_palacio && inicioData.imagenes_palacio.length > 0 ? (
+        <div className="relative">
+          <img
+            src={`http://127.0.0.1:8000${inicioData.imagenes_palacio[0].imagen}`}  
+            alt="Municipalidad de Cantel"
+            className="w-full h-[60vh] object-cover cursor-pointer"
+            onClick={() => openModal(inicioData.imagenes_palacio, 0)}  
+          />
+          <div className="absolute bottom-0 left-0 p-4 text-white bg-gradient-to-t from-black via-transparent to-transparent">
+            <h1 className="text-xl md:text-2xl lg:text-4xl font-semibold">
+              {inicioData.eslogan}
+            </h1>
+            <p className="text-md md:text-lg lg:text-xl mt-1">
+              {inicioData.administracion}
+            </p>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="relative">
+          <p>No hay imágenes disponibles del Palacio Municipal.</p>
+        </div>
+      )}
 
+      
       {/* Descripción */}
       <div className="p-8 text-center">
         <p className="text-base md:text-lg lg:text-xl text-gray-800">
-          {inicioData.descripcion}  {/* Descripción */}
+          {inicioData.descripcion}
         </p>
-
+        
         <p className=" p-8 text-center text-md md:text-lg lg:text-xl mt-1">
           {inicioData.administracion}  {/* Administración */}
         </p>
+        
       </div>
-      
 
       {/* Sección de noticias importantes */}
       <div className="p-8">
-        
-
         <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4">
           <div className="bg-gray-100 rounded-lg p-4 flex items-center justify-center">
             <p className="bg-gray-200 text-base md:text-lg font-bold py-2 px-6 rounded-full">
@@ -98,16 +97,15 @@ const Inicio = () => {
           </div>
         </div>
 
-        {/* Mostrar las 3 noticias más recientes */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
           {noticiasRecientes.map((noticia, index) => (
             <div key={noticia.id} className="bg-white shadow-md rounded-lg p-4">
               {noticia.imagenes && noticia.imagenes.length > 0 && (
                 <img
-                  src={`http://127.0.0.1:8000${noticia.imagenes[0].imagen}`} // Mostrar la primera imagen directamente
+                  src={`http://127.0.0.1:8000${noticia.imagenes[0].imagen}`} 
                   alt={noticia.titulo}
                   className="w-full h-48 object-cover rounded-md mb-4 cursor-pointer"
-                  onClick={() => openModal(noticia.imagenes, 0)}  // Abre el modal y pasa las imágenes
+                  onClick={() => openModal(noticia.imagenes, 0)}  
                 />
               )}
               <h3 className="text-lg font-bold mb-2">{noticia.titulo}</h3>
@@ -119,7 +117,6 @@ const Inicio = () => {
         </div>
       </div>
 
-      {/* Modal para mostrar la imagen en grande */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
           <div className="relative bg-white p-4 rounded-lg shadow-lg">
@@ -127,7 +124,7 @@ const Inicio = () => {
               src={`http://127.0.0.1:8000${currentImages[currentImageIndex].imagen}`}
               alt="Imagen ampliada"
               className="max-w-full max-h-[80vh] object-contain cursor-pointer mb-4"
-              onClick={handleNextImage}  // Cambia a la siguiente imagen al hacer clic
+              onClick={handleNextImage}
             />
             <button
               onClick={closeModal}
